b3b055593faed825cab07077bc026bda09b29912
vadim kotlyarov <vadimkotlyarov1990@gmail.com>
2023-06-04 22:49:10 +0300

    lesson05

---
+++ lesson05/lesson05.js
@@ -0,0 +1,20 @@
+let str = "1"
+let num = 2
+let bool = true
+
+console.log(str+bool);
+console.log(str+num);
+console.log(num+bool);
+console.log(str*bool);
+console.log(str*num);
+console.log(num*bool);
+console.log(str/bool);
+console.log(str/num);
+console.log(num/bool);
+
+console.log(String(num));
+console.log(String(bool));
+console.log(Number(str));
+console.log(Number(bool));
+console.log(Boolean(str));
+console.log(Boolean(num));
